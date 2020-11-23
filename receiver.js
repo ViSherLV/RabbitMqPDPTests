d3b5ca1446dd941141922748cdaa4bc3ef170f6f
Your Name <vishersb@gmail.com>
2020-11-24 01:08:41 +0200

    work with exchange

--- receiver.js
+++ receiver.js
@@ -1,17 +1,20 @@
-const amqp = require('amqplib/callback_api');
-amqp.connect('amqp://localhost', (connectError, connection) => {
-    if (connectError) {
-        throw connectError
+const amqp = require("amqplib/callback_api");
+amqp.connect("amqp://localhost", (connectError, connection) => {
+  if (connectError) {
+    throw connectError;
+  }
+  connection.createChannel((channelError, channel) => {
+    if (channelError) {
+      throw connectError;
     }
-    connection.createChannel((channelError, channel) => {
-        if (channelError) {
-            throw connectError
-        }
-        const QUEUE = 'victortest'
-        channel.assertQueue(QUEUE);
+    const QUEUE = "victortest";
+    let options = {
+      headers: { test: "test", test2: "test2", "x-match": "all" },
+    };
+    channel.assertQueue(QUEUE, options);
 
-        channel.consume(QUEUE, (msg) => {
-            console.log(`Message received: ${msg.content}`)
-        });
-    })
-})
+    channel.consume(QUEUE, (msg) => {
+      console.log(`Message received: ${msg.content}`);
+    });
+  });
+});
--- sender.js
+++ sender.js
@@ -1,17 +1,28 @@
-const amqp = require('amqplib/callback_api');
+const amqp = require("amqplib/callback_api");
 //creating connection
-amqp.connect('amqp://localhost', (connectError, connection) => {
-    if (connectError) {
-        throw connectError
+//http://localhost:15672/#/exchanges
+amqp.connect("amqp://localhost", (connectError, connection) => {
+  if (connectError) {
+    throw connectError;
+  }
+  connection.createChannel((channelError, channel) => {
+    if (channelError) {
+      throw connectError;
     }
-    connection.createChannel((channelError, channel) => {
-        if (channelError) {
-            throw connectError
-        }
-        const QUEUE = 'victortest'
-        channel.assertQueue(QUEUE);
-        channel.sendToQueue(QUEUE, Buffer.from('hello from Victor'));
-        console.log(`Message sended ${QUEUE}`)
-    })
+    // channel.ExchangeDeclare(
+    //     exchange: "Victor exchange",    //name of exchange
+    //     type: "direct",             //type (direct, fanout, headers, topic)
+    //     durable: "false",           // Durability (if false - deleted when server reload)
+    //     autoDelete: "false",
+    //     arguments: null              //another exchange, if first route not work
+    //     internal: "no",              // "yes" only for E2E
+    // );
+    const QUEUE = "victortest";
+    channel.assertQueue(QUEUE);
+    let options = {
+      headers: { test: "test", test2: "test2", "x-match": "all" },
+    };
+    channel.sendToQueue(QUEUE, Buffer.from("hello from Victor"), options);
+    console.log(`Message sended ${QUEUE}`);
+  });
 });
-
